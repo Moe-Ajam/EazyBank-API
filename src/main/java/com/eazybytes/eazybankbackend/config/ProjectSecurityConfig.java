4a651fcfe11e86652638ad65111be74c9f36713a
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2024-01-20 15:41:11 +0300

    Connected to the database and configured Spring Security to read users from the database. (default JDBC implementation).

--- src/main/java/com/eazybytes/eazybankbackend/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/eazybankbackend/config/ProjectSecurityConfig.java
@@ -3,7 +3,16 @@ package com.eazybytes.eazybankbackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,6 +28,20 @@ public class ProjectSecurityConfig {
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    /**
+     * NoOpPasswordEncoder is not recommended for production usage.
+     * @return PasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance ();
     }
 
 }
